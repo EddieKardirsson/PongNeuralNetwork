f64fb851baac101cdf02df342211817caaabf29e
Edison Kardirsson <ed.kardirsson@hotmail.com>
2021-10-24 22:49:46 +0200

    final

---
+++ Assets/MoveBall.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBall : MonoBehaviour {
+
+    Vector3 ballStartPosition;
+    Rigidbody2D rb;
+    [Range(400,800)]
+    public float speed = 400;
+    public AudioSource blip;
+    public AudioSource blop;
+
+	// Use this for initialization
+	void Start ()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        ballStartPosition = transform.position;
+        ResetBall();
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "backwall")
+            blop.Play();
+        else
+            blip.Play();
+    }
+
+    private void ResetBall()
+    {
+        transform.position = ballStartPosition;
+        rb.velocity = Vector3.zero;
+        Vector3 dir = new Vector3(Random.Range(100, 300), Random.Range(-100, 100), 0).normalized;
+        rb.AddForce(dir * speed);
+    }
+
+    // Update is called once per frame
+	void Update () 
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            ResetBall();
+        }
+    }
+}
---
+++ Assets/PlayerPaddle.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPaddle : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private BoxCollider2D col;
+    float yvel;
+    float paddleMinY = 8.8f;
+    float paddleMaxY = 17.4f;
+    float paddleMaxSpeed = 15;
+    public float numSaved = 0;
+    public float numMissed = 0;
+    private Vector3 direction;
+
+    [Range(1,20)]
+    public float paddleSpeed = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        col = GetComponent<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float posy = Mathf.Clamp(rb.transform.position.y + (yvel * Time.deltaTime * paddleSpeed),
+            paddleMinY, paddleMaxY);
+
+        float input = Input.GetAxisRaw("Vertical");
+        float translation = Time.deltaTime * input * paddleSpeed;
+        if (input > 0)
+        {
+            if (transform.position.y >= paddleMaxY)
+                direction = Vector3.zero;
+            else
+                direction = Vector3.up;
+        }
+        else if (input < 0)
+        {
+            if (transform.position.y <= paddleMinY)
+                direction = Vector3.zero;
+            else
+                direction = Vector3.down;
+        }
+        else direction = Vector3.zero;
+        
+        transform.Translate(0, direction.y*paddleSpeed*Time.deltaTime, 0);
+    }
+
+    void FixedUpdate()
+    {
+        rb.AddForce(direction * paddleSpeed);
+    }
+}
